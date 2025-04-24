ad68ac879117c6bad5923623a3e4374c55f2b9ab
ZIBA99 <97379804+ZIBA99@users.noreply.github.com>
2025-04-24 13:43:06 +0900

    Add files via upload

---
+++ Operator1.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+// b = a++, a==1,b==0
+//b =++a, a=1,b=1
+//int main(void)
+/*{
+	int num1 =12;
+	int num2 = 12;
+
+	printf("num1: %d \n", num1);
+	printf("num1++: %d \n", num1++);
+	printf("num1: %d \n\n", num1);
+
+	printf("num2: %d \n", num2);
+	printf("num2++: %d \n", ++num2);
+	printf("num2: %d \n\n", num2);
+	return 0;
+}*/
